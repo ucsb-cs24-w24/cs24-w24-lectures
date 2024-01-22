e5fbbf9088a3ab9e7c3ccffc0e8dc7821e1f8aba
Diba Mirza <dimirza@cs.ucsb.edu>
2024-01-22 10:15:35 -0800

    lect 04 starter

--- lect04/bst.cpp
+++ lect04/bst.cpp
@@ -26,67 +26,9 @@ class IntList{
         Node* next;
     };
     Node* head;
-    Node* reverse(Node* h);
 };
-
-void IntList::clear(){
-
-}
-
-int IntList::sum() const{
-    return 42;
-
-}
-int IntList::front() const{
-    return 42;
-}
-int IntList::back() const{
-    return 42;
-
-}
-void IntList::print() const{
-    Node* p = head;
-    while(p){
-        cout << "["<< p->data << "]";
-        p = p->next;
-        if(p) cout << "->";
-    }
-    cout << endl;
-
-}
-typename IntList::Node* IntList::reverse(Node* h){
-    if(!h) return nullptr;
-    if(!h->next) return h;
-    Node* new_head = reverse(h->next);
-    h->next->next = h;
-    h->next = nullptr;
-    return new_head; 
-}
-
-// mutators or setter
-void IntList::reverse(){
-    head = reverse(head);
-}
-void IntList::push_front(int value){
-    head = new Node{value, head};
-}
-
-void IntList::pop_front(){
-
-}
-void IntList::erase(int value){
-
-}
 
 
 int main() {
-    IntList ll; // object of the class
-    ll.push_front(10);
-    ll.push_front(20);
-    ll.push_front(30);
-    ll.push_front(40);
-    ll.print();
-    ll.reverse();
-    ll.print();
     return 0;
 }
